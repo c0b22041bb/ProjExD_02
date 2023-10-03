9c074900872a226c8401d88c3dcbf01ea83c0852
Aoto <c0b22041bb@edu.teu.ac.jp>
2023-10-03 14:24:07 +0900

    練習2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -13,17 +13,16 @@ def main():
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)#二倍に拡大
     bd_img= pg.Surface((20, 20))#爆弾Surfaceをつくる
-    bd_img.set_colorkey((0,0,0))
+    bd_img.set_colorkey((0,0,0))#黒を透過させる
     pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)#中心に半径10の赤い円
     bd_rct = bd_img.get_rect() #SurfaceからRectを抽出する
     x,y = random.randint(0,WIDTH),random.randint(0,HEIGHT)
     bd_rct.center = (x,y)#Rectにランダムな座標を設定する
-
-
-
+    vx,vy = +5,+5
 
     clock = pg.time.Clock()
     tmr = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -31,10 +30,11 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        bd_rct.move_ip(vx,vy) #練習2　爆弾を移動させる
         screen.blit(bd_img,bd_rct)#練習1　Rectを使って試しにblit
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        clock.tick(50)
 
 
 if __name__ == "__main__":
