0ecafd01d0f9c5d78bdc4b7a83e6e96df31cc519
Aoto <c0b22041bb@edu.teu.ac.jp>
2023-10-03 14:17:38 +0900

    練習1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,3 +1,4 @@
+import random
 import sys
 import pygame as pg
 
@@ -10,7 +11,17 @@ def main():
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
-    kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)#二倍に拡大
+    bd_img= pg.Surface((20, 20))#爆弾Surfaceをつくる
+    bd_img.set_colorkey((0,0,0))
+    pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)#中心に半径10の赤い円
+    bd_rct = bd_img.get_rect() #SurfaceからRectを抽出する
+    x,y = random.randint(0,WIDTH),random.randint(0,HEIGHT)
+    bd_rct.center = (x,y)#Rectにランダムな座標を設定する
+
+
+
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -20,6 +31,7 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        screen.blit(bd_img,bd_rct)#練習1　Rectを使って試しにblit
         pg.display.update()
         tmr += 1
         clock.tick(10)
