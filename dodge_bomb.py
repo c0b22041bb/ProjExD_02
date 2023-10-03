a6b96f703b257bdecf8e0d40ef3aea80516d006f
Aoto <c0b22041bb@edu.teu.ac.jp>
2023-10-03 15:10:08 +0900

    練習3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -5,13 +5,38 @@ import pygame as pg
 
 WIDTH, HEIGHT = 1600, 900
 
+delta = { #移動量タプル
+    pg.K_UP: (0,-5),
+    pg.K_DOWN: (0,+5),
+    pg.K_LEFT: (-5,0),
+    pg.K_RIGHT: (+5,0)
+}
+
+def check_boung(obj_rct: pg.Rect):#pg.Rectのクラスのインスタンスが来ることを指定
+    """
+    引数：こうかとんRectかばくだんRect
+    戻り値：タプル(横方向判定効果、縦方向判定効果)
+    画面内ならTrue:画面外ならFalse
+    """
+    yoko,tate = True,True
+
+    if obj_rct.left < 0 or WIDTH < obj_rct.right:#横方向判定
+        yoko = False
+    if obj_rct.top < 0 or HEIGHT < obj_rct.bottom:#縦方向判定
+        tate = False
+    return yoko,tate
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
+    """こうかとん"""
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)#二倍に拡大
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = (900,400)
+
+    """ばくだん"""
     bd_img= pg.Surface((20, 20))#爆弾Surfaceをつくる
     bd_img.set_colorkey((0,0,0))#黒を透過させる
     pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)#中心に半径10の赤い円
@@ -29,7 +54,20 @@ def main():
                 return
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
+        """こうかとん"""
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0,0]
+        for key, mv in delta.items():
+            if key_lst[key] :#keyが押されていたら
+                sum_mv[0] += mv[0] #横方向の合計移動量
+                sum_mv[1] += mv[1] #縦方向の合計移動量
+
+
+        kk_rct.move_ip(sum_mv[0],sum_mv[1])
+        screen.blit(kk_img,kk_rct)
+
+
+        """ばくだん"""
         bd_rct.move_ip(vx,vy) #練習2　爆弾を移動させる
         screen.blit(bd_img,bd_rct)#練習1　Rectを使って試しにblit
         pg.display.update()
